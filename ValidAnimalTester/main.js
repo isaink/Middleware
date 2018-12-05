7a02264401a9a8ce739fc6426966328fd60412a7
isaink <inks.minds@gmail.com>
2018-12-05 15:26:54 -0500

    validAnimals

--- ValidAnimalTester/main.js
+++ ValidAnimalTester/main.js
@@ -15,28 +15,25 @@ let response = {
 };
 
 const isAnimal = (req, res, next) => {  //checking if the query have the same animalroute on my objAnimal...
-  // req.message = "
+
+  req.plannedRes = {
+    status: "failure",
+    message: false
+  }
+
   animalsKey.forEach(el => { //trying to check equality...
     // console.log(el)
     if(req.params.animal === el){
+      req.plannedRes = response;
       console.log('yep');
-      return true;
-    }else{
-      console.log('nope');
-      return false;
-    };
+    }
   });
   next();
 };
 
 app.get("/animal/:animal", isAnimal, (req, res) => { //query the url and converted to json....
-
-  res.json(req.params.animal); // "/animal/:animal",
-  console.log(req.params.animal)
+  res.json(req.plannedRes); // "/animal/:animal",
 });
-
-// app.get();
-// app.use(isAnimal);
 
 app.listen(1000, () => {
   console.log("listening to port 1000");
